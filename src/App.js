e51afeb70cdee59b39c6dde6c17867f292e8ba00
Tomas Laus <tomaslaus99@gmail.com>
2021-08-18 15:36:51 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Esta es mi aplicación . La</h1>
+        <h1>Esta es mi aplicación. Laus</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
