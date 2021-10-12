dca6315658c4e5a3692c9182e9de230ee4df5189
TomasLaus <tomaslaus99@gmail.com>
2021-10-12 18:25:33 -0300

    Ultima

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -53,36 +53,18 @@ function CartContextProvider({children}) {
 
    const mostrarItemsCarrito = () => {
    return product.map(pro =><div className='fade-up' style={{borderRadius: '16px'}}>
-    <div className="d-flex justify-content-center my-5">
-    <div className="card animate__bounceIn" style={{width: '30rem',padding:'10px'}}>
-      <button onClick={() => deleteFromCart(pro.item.id)} className='deleteItemCart'>X</button>
-      <img src={pro.item.imageUrl} alt="" className='cartImg' /> 
-      <div className="card-body">
-      <p className='cartTitle'>{ pro.item.title}</p>
-      <p className='cartQuantity'>Cantidad: { pro.quantity}</p> 
+      <div className="d-flex justify-content-center my-5">
+        <div className="card animate__bounceIn" style={{width: '30rem',padding:'10px'}}>
+          <button onClick={() => deleteFromCart(pro.item.id)} className='deleteItemCart'>X</button>
+          <img src={pro.item.imageUrl} alt="" className='cartImg' /> 
+          <div className="card-body">
+          <p className='cartTitle'>{ pro.item.title}</p>
+          <p className='cartQuantity'>Cantidad: { pro.quantity}</p> 
+          </div>
       </div>
-    </div>
     </div>
     </div>)
 
-
-/*            <div data-aos='fade-up'>
-            <div className="d-flex justify-content-center my-5">
-            <div className="card animate__bounceIn" style={{width: '30rem',padding:'10px'}}>
-            <button onClick={handleHistory} type="button" name="button" className="volver-detail"><FontAwesomeIcon icon={faArrowLeft} /></button>
-                <img src={item.imageUrl} alt={item.description} className="card-img-top border border-dark rounded "/>
-                <div className="card-body">
-                    <h5 className="card-title">{item.title}</h5>
-                    <p className="card-text">{item.description}</p>
-                </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">Precio: ${item.price}</li>
-                    <li className="list-group-item"><ItemCount initial={1} stock={5} onAdd={onAdd} /></li>
-
-                </ul>
-                </div>
-                </div>
-                </div> */
    }
 
    
